4cbc54a3f12350dff47d754f0207c0860c034731
mohammad <96320586+Mohammad-Bereymi@users.noreply.github.com>
2022-09-07 16:32:41 +0430

    app logic

---
+++ src/app.js
@@ -0,0 +1,22 @@
+buttons.forEach((btn) => {
+  btn.addEventListener("click", calculate);
+});
+let count = 0;
+
+function calculate(event) {
+  if (event.target.classList[1] == "decrement") {
+    counter.innerHTML = --count;
+  } else if (event.target.classList[1] == "reset") {
+    count = 0;
+    counter.innerHTML = count;
+  } else if (event.target.classList[1] == "increment") {
+    counter.innerHTML = ++count;
+  }
+  if (count < 0) {
+    counter.style.color = "red";
+  } else if (count > 0) {
+    counter.style.color = "green";
+  } else {
+    counter.style.color = "black";
+  }
+}
---
+++ src/dom-loader.js
@@ -0,0 +1,2 @@
+const counter = document.querySelector(".counter");
+const buttons = [...document.querySelectorAll(".btn")];
